120938075f561c31c3491e6ec46b096c81c8fea9
Dharmaraj <dharmarajbuzo@gmail.com>
2023-05-04 13:27:40 +0530

    Web pages responsive design 1920 1440 width

--- src/Webpage/Home.jsx
+++ src/Webpage/Home.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// import HorizontalScroll from 'react-horizontal-scrolling';
-// import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Lottie from "lottie-react";
@@ -8,12 +6,10 @@ import Musicico from "../components/musicico.json";
 import Eventmanager from '../assets/images/event_manager.png';
 import Guitarist from '../assets/images/guitarist.png';
 import Joinbg from '../assets/images/join-bg.png';
-
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import "react-horizontal-scrolling-menu/dist/styles.css";
 import { useState } from "react";
 import usePreventBodyScroll from "./bodyscroll";
-import { Card } from "./Card";
 
 
 const elemPrefix = "test";
@@ -45,39 +41,6 @@ const Home = () => {
 
   return (
     <>
-      {/* <div className="home-main-sec">
-        <div className="inner-home-main-sec">
-          <div className="section1 maindg postion-r"  style={{background: `url(${Joinbg})`}}>
-            <Row>
-              <Col lg={6}>
-                <div className="enjoy-sec">
-                  <p className="sub-head l-sb fs-2 red-color">We hope you enjoyed <br />this musical journey!!</p>
-                  <p className="heading khf  black-color l-m">Want to be a part of Livetunes <span className="red-color">Community?</span></p>
-                </div>
-              </Col>
-              <Col lg={6}>
-                <div className="musicico">
-                  <Lottie animationData={Musicico} loop={false} />
-                </div>
-              </Col>
-            </Row>
-
-            <img src={Eventmanager} alt="" className="eventmanager-img-sec"/>
-            <img src={Guitarist} alt="" className="guitarist-img-sec"/>
-          </div>
-
-          <div className="maindg bg2">
-            <h1>Hello 2</h1>
-          </div>
-          <div className="maindg bg3">
-            <h1>Hello 3</h1>
-          </div>
-          <div className="maindg bg4">
-            <h1>Hello 4</h1>
-          </div>
-        </div>
-    </div> */}
-
       <div className="example">
         <div onMouseEnter={disableScroll} onMouseLeave={enableScroll}>
           <ScrollMenu
@@ -94,59 +57,37 @@ const Home = () => {
             ))} */}
 
             {items[0] && 
-            
             <div className="home-main-sec">
                 <div className="inner-home-main-sec">
                   <div className="section1 maindg postion-r"  style={{background: `url(${Joinbg})`}}>
                     <Row>
-                      <Col lg={6}>
+                      <Col lg={7}>
                         <div className="enjoy-sec">
-                          <p className="sub-head l-sb fs-2 red-color">We hope you enjoyed <br />this musical journey!!</p>
-                          <p className="heading khf  black-color l-m">Want to be a part of Livetunes <span className="red-color">Community?</span></p>
+                          <p className="sub-head l-sb red-color">We hope you enjoyed <br />this musical journey!!</p>
+                          <p className="heading khf  black-color l-m">Want to be a part of Livetunes <span className="red-color">Community</span>?</p>
                         </div>
                       </Col>
-                      <Col lg={6}>
+                      <Col lg={5}>
                         <div className="musicico">
-                          <Lottie animationData={Musicico} loop={false} />
+                          <Lottie animationData={Musicico} loop={true} />
                         </div>
                       </Col>
                     </Row>
 
                     <img src={Eventmanager} alt="" className="eventmanager-img-sec"/>
+                    <div className="eventmanager-btn-sec">
+                      <button type="button" className="l-sb wbtnn back-btn btn btn-primary">Join as a <br /> Organiser</button>
+                    </div>
+
                     <img src={Guitarist} alt="" className="guitarist-img-sec"/>
+                    <div className="guitarist-btn-sec">
+                      <button type="button" className="l-sb wbtnn back-btn btn btn-primary">Join as <br /> an Artist</button>
+                    </div>
+
                   </div>
                 </div>
             </div>
-
             }
-
-            {items[1] && 
-            
-            <div className="home-main-sec">
-                <div className="inner-home-main-sec">
-                  <div className="section1 maindg postion-r"  style={{background: `url(${Joinbg})`}}>
-                    <Row>
-                      <Col lg={6}>
-                        <div className="enjoy-sec">
-                          <p className="sub-head l-sb fs-2 red-color">We hope you enjoyed <br />this musical journey!!</p>
-                          <p className="heading khf  black-color l-m">Want to be a part of Livetunes <span className="red-color">Community?</span></p>
-                        </div>
-                      </Col>
-                      <Col lg={6}>
-                        <div className="musicico">
-                          <Lottie animationData={Musicico} loop={false} />
-                        </div>
-                      </Col>
-                    </Row>
-
-                    <img src={Eventmanager} alt="" className="eventmanager-img-sec"/>
-                    <img src={Guitarist} alt="" className="guitarist-img-sec"/>
-                  </div>
-                </div>
-            </div>
-
-            }
-
 
           </ScrollMenu>
         </div>
