dccaf87f4c432a60b0040af5e1b9b53174319c74
amit.p <amit.p@youpalgroup.com>
2023-11-13 14:01:46 +0530

    judge fix

--- src/Judgment/VideoList.jsx
+++ src/Judgment/VideoList.jsx
@@ -11,7 +11,7 @@ const VideoList = (props) => {
       <Row>
         <Col lg={7}>
             <div className="left-video-sec">
-                <video width="100%" height="400px" src={props.videoData?.filter((key) => key.LTMediaURL.includes(".mp4"))[0].LTMediaURL} controls={true}></video>
+                <video width="100%" height="400px" src={props.videoData?.filter((key) => key.LTMediaURL.includes(".mp4"))[0]?.LTMediaURL} controls={true}></video>
                 <div className="video-tile-date">
                     <p className="l-r fs-4 mb-1">{props.videoData[0].MediaDesc != null ? props.videoData[0].MediaDesc : 'NA'}</p>
                     <p className="l-r fs-5">Date: {moment(props.videoData[0].AddDate).format("DD/MM/YYYY")}</p>
@@ -25,7 +25,7 @@ const VideoList = (props) => {
                         <div className="inner-video-sub-list" key={index}>
                             <Stack direction="horizontal"  className="align-items-self" gap={3}>
                                 <div className="video-sub-list">
-                                    <video width="100%" height="120" src={vid.LTMediaURL} controls={true}></video>
+                                    <video width="100%" height="120" src={vid?.LTMediaURL} controls={true}></video>
                                 </div>
                                 <div className="video-sub-list">
                                     <p className="l-sb head-title mb-2">{`${vid.MediaDesc != null ? vid.MediaDesc : 'NA'}`}</p>
@@ -40,7 +40,7 @@ const VideoList = (props) => {
                         <div className="inner-video-sub-list" key={index}>
                             <Stack direction="horizontal"  className="align-items-self" gap={3}>
                                 <div className="video-sub-list">
-                                    <img width="100%" height="120" src={vid.LTMediaURL} />
+                                    <img width="100%" height="120" src={vid?.LTMediaURL} />
                                 </div>
                                    <div className="video-sub-list">
                                         <p className="l-sb head-title mb-2">{`${vid.MediaDesc != null ? vid.MediaDesc : 'NA'}`}</p>
